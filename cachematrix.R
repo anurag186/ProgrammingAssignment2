2a1983af082d2b1758c859bd801cece27d86f6d2
anurag186 <38192313+anurag186@users.noreply.github.com>
2018-06-25 14:37:21 -0600

    Updates for week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create functions to cache the inverse of a matrix in memory and retrieve it when needed.
 
-## Write a short comment describing this function
 
+## The purpose of this function is to create an R object that stores a matrix 
+## and its inverse in the memory. This function initializes two objects, x 
+## and i. x is a matrix object and i is the inverse of matrix x. To get and
+## set the matrix, x and its inverse, i, four functions (two get and two set) are
+## nested within this function. This function then creates a list object holding
+## these get and set functions for the matrix object.
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list (set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function uses an object created using makeCacheMatrix(x) function as an 
+## argument and returns the inverse of the matrix, x. Function first attempts to
+## check the cached memory to find the inverse of x. If inverse matrix is null,
+## function computes the inverse and set the inverse for the makeCacheMatrix 
+## object.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()
+	i <- inv(data, ...)
+	x$setinverse(i)
+	i  
 }
+
+
+## execute the script using
+## A <- matrix( c(5, 1, 0, 3,-1, 2, 4, 0,-1), nrow=3, byrow=TRUE)
+## m <- makeCacheMatrix(A)	## create makeCacheMatrix object in cache
+## cacheSolve(m)			## since inverse for m doesn't exist in memory, this will compute the inverse
+## cacheSolve(m)			## this will get the inverse from cache.
